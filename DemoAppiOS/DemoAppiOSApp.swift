39f54718b3a9e20e85d69844f0acf8d9653e1dfb
vishu <itsvishuhanda@gmail.com>
2021-07-22 12:08:47 +0530

    first commit

---
+++ DemoAppiOS/DemoAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppiOSApp.swift
+//  DemoAppiOS
+//
+//  Created by Admin on 16/07/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
